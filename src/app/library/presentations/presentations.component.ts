d7664a25d353a56790f127fed5b830733a83f3f6
Phil Bennett <pbennett@benefitcommunications.com>
2018-04-04 10:35:11 -0500

    Presentations Page

--- src/app/library/presentations/presentations.component.ts
+++ src/app/library/presentations/presentations.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from '../../core/globals';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Page, Video } from '../../core/content-model';
+import { ContentService } from '../../core/content.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-presentations',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationsComponent implements OnInit {
 
-  constructor() { }
+  page: Page;
+  presentations: Video[];
 
-  ngOnInit() {
+  constructor(private contentService: ContentService,
+              private globals: Globals,
+              private route: ActivatedRoute,
+              private router: Router,
+              private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+
+    this.contentService.getPageObservable('presentations').subscribe(page => this.onPageResponse(page), error => this.router.navigate(['/page-not-found']));
+
+  }
+
+  onPageResponse(page: Page): void {
+    page.safeContent = this.sanitizer.bypassSecurityTrustHtml(page.content);
+
+    this.contentService.getSubPagesByIdObservable(page.id).subscribe((page: Video) => {
+      this.presentations = page.subpages;
+      console.log(this.presentations);
+    }, error => {
+
+    });
+
+    this.page = page;
+  }
+
+  sanitize(url:string){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 }
